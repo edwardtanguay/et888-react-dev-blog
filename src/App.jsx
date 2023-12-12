aecf550bd2649dbc59798de4606e0bda9166d6f8
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 01:21:43 +0100

    fixed blog/:id issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Documentation from './pages/Documentation'
 import Portfolio from './pages/Portfolio'
+import BlogPost from './components/BlogPost';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/documentation" element={<Documentation />} />
           <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
       </div>
     </Router>
